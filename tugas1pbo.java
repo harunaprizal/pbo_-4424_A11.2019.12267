5fcb5633d827ef9c185cbf652da0f7e8a0ba3618
harunaprizal <80037347+harunaprizal@users.noreply.github.com>
2021-03-10 09:11:38 +0700

    Add files via upload

---
+++ tugas1pbo.java
@@ -0,0 +1,29 @@
+
+//Harun Aprizal Widyapradana
+//A11.2019.12267
+
+public class tugas1pbo {
+    public static void main (String args []) {
+        int     a = 5,
+                b = 8,
+                c = 6,
+                d = 9,
+                e = 55,
+                f = 20,
+                g = 15,
+                h = 3,
+                i = 2 ;
+
+        int call1 = -a +b*c;
+        int call2 = (e+d)%d;
+        int call3 = f+(-h*a)/b;
+        int call4 = a+g/h*i-b%h;
+
+        System.out.println("HASIL PROGRAM :");
+        System.out.println("D. " +call1);
+        System.out.println("O. " +call2);
+        System.out.println("N. " +call3);
+        System.out.println("E. " +call4);
+    }
+
+}
